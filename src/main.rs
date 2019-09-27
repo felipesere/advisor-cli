6c15686bd44a95296ad90aae698efb7595063983
Felipe Sere <felipesere@gmail.com>
2019-09-27 10:33:32 +0100

    WIP: Try some form of parsers/matching thing. Does not compile

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::time::Duration;
 
 use futures_timer::ext::TryFutureExt;
 use serde::Deserialize;
-use clap::{Arg, App};
+use clap::{arg_enum, value_t, Arg, App};
 use snafu::Snafu;
 
 type MyResult<T> = Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
@@ -27,6 +27,10 @@ struct AdvisorApp {
 impl AdvisorApp {
     fn healthcheck(&self) -> String {
         format!("{}/healthcheck", self.location)
+    }
+
+    fn list_people(&self) -> String {
+        format!("{}/admin/people", self.location)
     }
 }
 
@@ -54,25 +58,82 @@ fn load_config() -> SnafuResult<Config> {
     settings.try_into::<Config>().or_else(|_| CouldNotFindConfig.fail())
 }
 
+type PersonParams = std::collections::HashMap<String, String>;
+
+
+#[derive(Eq, PartialEq, Debug)]
+enum Command {
+    ShowQuestionnaires,
+    ShowPeople,
+    DeletePerson{email: String},
+    CreatePerson(PersonParams),
+    AddPersonToQuestionnaire{id: String, email: String},
+    RemovePersonFromQuestionnaire{id: String, email: String},
+    Unknown(Vec<String>),
+}
+
+impl Command {
+    fn parse(arguments: Vec<String>) -> Command {
+        use Command::*;
+
+        if arguments == vec!("show".to_string(), "people".to_string()) {
+            return ShowPeople;
+        }
+
+        if arguments == vec!("show".to_string(), "questionnares".to_string()) {
+            return ShowQuestionnaires;
+        }
+
+        if arguments.get(0) == Some(&"delete".to_string()) {
+            if let Some(email) = arguments.get(1) {
+                return DeletePerson { email: email.clone() }
+            }
+        }
+
+        let add = "add".to_string();
+        let remove = "remove".to_string();
+
+        if arguments.get(0) == Some(&"update".to_string()) {
+            match arguments.get(2) {
+                Some(a) if a == &add => return AddPersonToQuestionnaire { id: arguments[1].clone(), email: arguments[3].clone() },
+                Some(a) if a == &remove => return RemovePersonFromQuestionnaire { id: arguments[1].clone(), email: arguments[3].clone() },
+                None | Some(_) => (),
+            }
+        }
+
+        if arguments.get(0) == Some(&"create".to_string()) && arguments.get(1) == Some(&"person".to_string()) {
+            let remainder = arguments.iter().skip(2).collect::<Vec<_>>();
+            for chunk in remainder.chunks_exact(2) {
+            }
+        }
+
+        Unknown(arguments)
+    }
+}
+
 
 #[runtime::main]
 async fn main() -> MyResult<()> {
     let matches = App::new("Advisor-CLI")
-                          .version("0.1")
-                          .author("Felipe Sere felipe@sere.dev>")
-                          .about("Managing instances of advisor")
-                          .arg(Arg::with_name("app_name")
-                               .short("a")
-                               .long("app")
-                               .value_name("APP")
-                               .help("Which app to act upon")
-                               .required(true)
-                               .takes_value(true))
-                          .get_matches();
+        .version("0.1")
+        .author("Felipe Sere felipe@sere.dev>")
+        .about("Managing instances of advisor")
+        .arg(Arg::with_name("app_name")
+            .short("a")
+            .long("app")
+            .value_name("APP")
+            .help("Which app to act upon")
+            .required(true)
+            .takes_value(true))
+        .get_matches();
 
-    let app_name = matches.value_of("app_name").unwrap();
+    let args: Vec<String> = std::env::args().collect();
+
+    let command = Command::parse(args);
 
     let config = load_config().unwrap();
+
+    let app_name = matches.value_of("app_name").unwrap();
 
     let app = config.for_app(app_name).unwrap();
 
@@ -82,4 +143,69 @@ async fn main() -> MyResult<()> {
     }
 
     Ok(())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    macro_rules! strings {
+        ($($x:expr),*) => (vec![$($x.to_string()),*]);
+    }
+
+
+    #[test]
+    fn test_parse_unknown() {
+        let command = Command::parse(strings!("foo", "bar"));
+
+        assert_eq!(command, Command::Unknown(strings!("foo", "bar")))
+    }
+
+    #[test]
+    fn test_parse_show_people() {
+        let command = Command::parse(strings!("show", "people"));
+
+        assert_eq!(command, Command::ShowPeople)
+    }
+
+    #[test]
+    fn test_parse_show_questionnaires() {
+        let command = Command::parse(strings!("show", "questionnares"));
+
+        assert_eq!(command, Command::ShowQuestionnaires)
+    }
+
+    #[test]
+    fn test_parse_delete_person() {
+        let command = Command::parse(strings!("delete", "a@b.com"));
+
+        assert_eq!(command, Command::DeletePerson{email: "a@b.com".to_string()})
+    }
+
+    #[test]
+    fn test_add_person_to_questionnaire() {
+        let command = Command::parse(strings!("update", "123a", "add", "a@b.com"));
+
+        assert_eq!(command, Command::AddPersonToQuestionnaire{id: "123a".to_string(), email: "a@b.com".to_string()})
+    }
+
+    #[test]
+    fn test_remove_person_from_questionnaire() {
+        let command = Command::parse(strings!("update", "123a", "remove", "a@b.com"));
+
+        assert_eq!(command, Command::RemovePersonFromQuestionnaire{id: "123a".to_string(), email: "a@b.com".to_string()})
+    }
+
+    #[test]
+    fn test_create_person() {
+        let command = Command::parse(strings!("create", "person", "--email", "a@b.com", "--name", "Steve"));
+
+        let mut params: HashMap<String, String> = HashMap::new();
+        params.insert("name".to_string(), "Steve".to_string());
+        params.insert("email".to_string(), "a@b.com".to_string());
+
+        assert_eq!(command, Command::CreatePerson(params))
+    }
 }
